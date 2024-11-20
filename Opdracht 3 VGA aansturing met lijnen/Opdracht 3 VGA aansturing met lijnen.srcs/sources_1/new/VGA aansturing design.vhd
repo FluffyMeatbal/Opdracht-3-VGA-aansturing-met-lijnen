f8ff78d1051a1e98669357d2c2c5a2d2dd769361
FluffyMeatbal <remcopieper05@hotmail.com>
2024-11-20 22:03:18 +0100

    Hsync, Vsync and VideoON added

--- Opdracht 3 VGA aansturing met lijnen/Opdracht 3 VGA aansturing met lijnen.srcs/sources_1/new/VGA aansturing design.vhd
+++ Opdracht 3 VGA aansturing met lijnen/Opdracht 3 VGA aansturing met lijnen.srcs/sources_1/new/VGA aansturing design.vhd
@@ -33,28 +33,32 @@ use IEEE.Numeric_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity VGA_aansturing is
-Port (clk: in std_logic);
+Port (clk: in std_logic; Hsync, Vsync, video_ON: out std_logic);
 end VGA_aansturing;
 
 architecture Behavioral of VGA_aansturing is
-signal deler : integer := 0;
-signal sclk: std_logic;
-constant deelFactor : integer := 4;
-signal EN : std_logic;
+signal deler : integer := 0;                --signaal voor de deler
+signal sclk: std_logic;                     --gedeelde klok 
+constant prscl : integer := 4;              --prescaler
+signal EN : std_logic;                      --Enable
 
 signal xTel : integer range 0 to 1023 := 0;
 signal yTel : integer range 0 to 1023 := 0;
 signal lineAdvance : std_logic;
-signal video_ON : std_logic;
+
+signal H_sync : std_logic;
+signal V_sync : std_logic;
+signal vid_ON : std_logic;
+
 begin
 
 delerBlok: process(clk)
 begin
     if rising_edge(clk) then
-        if deler = deelFactor/2 then
+        if deler = prscl/2 then        --50% van periode voorbij
             sclk <= '1'; deler <= deler + 1;
         else
-            if deler = deelFactor then
+            if deler = prscl then      --100% van periode voorbij
                 sclk <= '0'; deler <= 0;
             else
                 deler <= deler + 1;
@@ -63,7 +67,7 @@ begin
     end if;
 end process delerBlok;
 
-X_teller: process(clk)
+X_teller: process(clk, xTel, lineAdvance)
 begin
     if rising_edge(clk) then
         if rising_edge(sclk) then
@@ -76,11 +80,11 @@ begin
     end if;
 end process X_teller;
 
-Y_Teller: process(clk)
+Y_Teller: process(clk, yTel, lineAdvance)
 begin
     if rising_edge(clk) then
-        if video_ON = '1' then
-            if lineAdvance = '0' then
+        if vid_ON = '1' then
+            if lineAdvance = '1' then
                 if yTel < 525 then
                    yTel <= yTel + 1;
                 else 
@@ -90,6 +94,27 @@ begin
         end if;
     end if;
 end process Y_teller;
+
+HORsync: process(xTel)
+begin
+    if xTel > 655 and xTel < 752 then
+        Hsync <= '0'; H_sync <= '0';
+    elsif xTel < 640 then
+        Hsync <= '1'; H_sync <= '0';
+    end if;
+end process HORsync;
+
+VERTsync: process(yTel)
+begin
+    if yTel > 489 and yTel < 492 then
+        Vsync <= '0'; V_sync <= '0';
+    elsif yTel < 480 then
+        Vsync <= '1'; V_sync <= '0';
+    end if;
+end process VERTsync;
+
+video_ON <= V_sync and H_sync;
+vid_ON <= V_sync and H_sync;
 
 end Behavioral;
 
