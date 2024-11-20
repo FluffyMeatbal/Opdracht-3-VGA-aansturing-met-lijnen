d39c2512bac019a282945c88e366bfb554b1269a
FluffyMeatbal <remcopieper05@hotmail.com>
2024-11-20 21:13:30 +0100

    clk div, x and y counters added

--- Opdracht 3 VGA aansturing met lijnen/Opdracht 3 VGA aansturing met lijnen.srcs/sources_1/new/VGA aansturing design.vhd
+++ Opdracht 3 VGA aansturing met lijnen/Opdracht 3 VGA aansturing met lijnen.srcs/sources_1/new/VGA aansturing design.vhd
@@ -32,14 +32,64 @@ use IEEE.Numeric_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity Teller is
-Port (clk, Reset: in std_logic;
-Q: std_logic_vector(8 downto 0));
-end Teller;
+entity VGA_aansturing is
+Port (clk: in std_logic);
+end VGA_aansturing;
 
-architecture Behavioral of Teller is
+architecture Behavioral of VGA_aansturing is
+signal deler : integer := 0;
+signal sclk: std_logic;
+constant deelFactor : integer := 4;
+signal EN : std_logic;
 
+signal xTel : integer range 0 to 1023 := 0;
+signal yTel : integer range 0 to 1023 := 0;
+signal lineAdvance : std_logic;
+signal video_ON : std_logic;
 begin
 
+delerBlok: process(clk)
+begin
+    if rising_edge(clk) then
+        if deler = deelFactor/2 then
+            sclk <= '1'; deler <= deler + 1;
+        else
+            if deler = deelFactor then
+                sclk <= '0'; deler <= 0;
+            else
+                deler <= deler + 1;
+            end if;
+        end if;
+    end if;
+end process delerBlok;
+
+X_teller: process(clk)
+begin
+    if rising_edge(clk) then
+        if rising_edge(sclk) then
+            if xTel < 800 then
+                xTel <= xTel + 1; lineAdvance <= '0';
+            else
+                xTel <= 0; lineAdvance <= '1';
+            end if;
+        end if;
+    end if;
+end process X_teller;
+
+Y_Teller: process(clk)
+begin
+    if rising_edge(clk) then
+        if video_ON = '1' then
+            if lineAdvance = '0' then
+                if yTel < 525 then
+                   yTel <= yTel + 1;
+                else 
+                   yTel <= 0;
+                end if;
+            end if;
+        end if;
+    end if;
+end process Y_teller;
 
 end Behavioral;
+
