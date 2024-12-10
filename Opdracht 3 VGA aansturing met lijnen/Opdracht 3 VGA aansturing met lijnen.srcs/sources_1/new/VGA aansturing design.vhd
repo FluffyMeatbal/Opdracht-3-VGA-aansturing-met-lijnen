7f2beaa1348de62517ffc7899de648c6bc10bf0c
FluffyMeatbal <remcopieper05@hotmail.com>
2024-12-10 14:58:33 +0100

    deel 2 is klaar

--- Opdracht 3 VGA aansturing met lijnen/Opdracht 3 VGA aansturing met lijnen.srcs/sources_1/new/VGA aansturing design.vhd
+++ Opdracht 3 VGA aansturing met lijnen/Opdracht 3 VGA aansturing met lijnen.srcs/sources_1/new/VGA aansturing design.vhd
@@ -42,15 +42,21 @@ use IEEE.Numeric_STD.ALL;
 entity VGA_aansturing is
 Port (
 clk: in std_logic;
-Red, Green, Blue: in std_logic_vector(3 downto 0);
+btnU, btnD: in std_logic;
 Hsync, Vsync, video_ON: out std_logic;
 vgaRed, vgaGreen, vgaBlue: out std_logic_vector(3 downto 0)
 );
 end VGA_aansturing;
 
 architecture Behavioral of VGA_aansturing is
-signal enable: std_logic;                             --gedeelde klok 
-constant prscl : integer := 4;                      --prescaler
+signal enable: std_logic;                               --gedeelde klok 
+constant prscl : integer := 4;                          --prescaler
+constant tweeHzSCL : integer := 50000000;               --prescaler voor 2Hz teller
+
+signal tweeHz : std_logic;                              --2Hz klok signaal
+signal btnUreg : std_logic;                             --register voor omhoog knop
+signal btnDreg : std_logic;                             --register voor omlaag knop
+signal lijnTel: integer range 0 to 511 := 0;            --teller voor de lijn
 
 signal xTel : integer range 0 to 1023 := 0;
 signal yTel : integer range 0 to 1023 := 0;
@@ -59,14 +65,16 @@ signal H_sync : std_logic;
 signal V_sync : std_logic;
 signal vid_ON : std_logic;
 
+signal Red, Green, Blue: std_logic_vector(3 downto 0);  --signaal voor kleuren
+
 begin
 
 delerBlok: process(clk)
-variable deler : integer range 0 to 511 := 1;          --variabele voor de deler
+variable deler : integer range 0 to 511 := 1;           --variabele voor de deler
 
 begin
     if rising_edge(clk) then
-        if deler = prscl / 4 then                   --25% van periode voorbij 
+        if deler = prscl / 4 then                       --25% van periode voorbij 
             enable <= '1';
         else
             enable <= '0';
@@ -159,6 +167,83 @@ end process RGBsync;
 Hsync <= H_sync;
 Vsync <= V_sync;
 video_ON <= vid_ON;
+
+miniCLK: process(clk)
+variable deler: integer range 0 to tweeHzSCL-1 := 0;
+begin
+if rising_edge(clk) then
+    if deler < tweeHzSCL then
+        deler := deler + 1;
+    else
+        deler := 0;
+    end if;
+    
+    if deler < tweeHzSCL / 2 then
+        tweeHz <= '1';
+    else
+        tweeHz <= '0';
+    end if;
+end if;
+end process miniCLK;
+
+knopRegistratie: process(clk, btnU, btnD)
+begin
+if rising_edge(clk) then
+    if btnU = '1' xor btnD = '1' then
+        btnUreg <= btnU;
+        btnDreg <= btnD;
+    end if;
+end if;
+end process knopRegistratie;
+
+lijnTeller: process(tweeHz, btnUreg, btnDreg)
+begin
+if rising_edge(tweeHz) then
+    if btnUreg = '1' then
+        if lijnTel < 480 - 44 then
+            lijnTel <= lijnTel + 1;
+        else
+            lijnTel <= 0;
+        end if;
+    end if;
+    if btnDreg = '1' then
+        if lijnTel > 0 then
+            lijnTel <= lijnTel - 1;
+        else
+            lijnTel <= 480 - 44;
+        end if;
+    end if;
+end if;
+end process lijnTeller;
+
+lijnen: process(clk, enable, lijnTel, yTel)
+begin
+if rising_edge(clk) then
+    if enable = '1' then
+        if yTel >= lijnTel and yTel < lijnTel +4 then
+            Red <= "1111";
+            Green <= "0000";
+            Blue <= "0000";
+        
+        elsif yTel >= lijnTel + 20 and yTel < lijnTel + 24 then
+            Red <= "1111";
+            Green <= "0000";
+            Blue <= "0000";
+        
+        elsif yTel >= lijnTel + 40 and yTel < lijnTel + 44 then
+            Red <= "1111";
+            Green <= "0000";
+            Blue <= "0000";
+        
+        else 
+            Red <= "1111";
+            Green <= "1111";
+            Blue <= "0000";
+            
+        end if;
+    end if;
+end if;
+end process lijnen;
 
 end Behavioral;
 
